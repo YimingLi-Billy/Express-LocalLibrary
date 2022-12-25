f9fe766173ccbec22493a1987d7f483beacffcbc
Billy <ysqj1hg@gmail.com>
2022-12-25 02:21:42 -0500

    changed mongoDB uri

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ dotenv.config({ path: "./.env" });
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGO_URI;
+const dev_db_url =
+  "mongodb+srv://scratchingenius:ysqj1hg@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
